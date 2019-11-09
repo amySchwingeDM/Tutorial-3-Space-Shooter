f6662787bb0c72c1be6248c2de07e5fb18a69dbe
amySchwingeDM <54595377+amySchwingeDM@users.noreply.github.com>
2019-11-08 21:19:49 -0500

    Add files via upload

---
+++ DestroybyBoundary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroybyBoundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
